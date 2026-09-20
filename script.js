0ec7adacbb6b5695fb021da0d392a7f95c747a87
shadow <shihabazizofficial@gmail.com>
2026-09-21 01:46:29 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSurprise() {
+
+    alert(
+        "🎉 Happy Birthday Jesmin! 🎂❤️\n\n" +
+        "May your life always be filled " +
+        "with happiness, love and beautiful memories. 💗"
+    );
+
+}
